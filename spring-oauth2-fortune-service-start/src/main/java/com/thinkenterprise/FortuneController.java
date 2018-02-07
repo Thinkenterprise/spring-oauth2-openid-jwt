8ee655dd9f333112a7293c21fe68e6f340c881ad
schaefem <schaefem@MSGN04551.int.root.msg.ag>
2018-02-07 20:05:29 +0100

    Add service start implementation

---
+++ spring-oauth2-fortune-service-start/src/main/java/com/thinkenterprise/FortuneController.java
@@ -0,0 +1,31 @@
+package com.thinkenterprise;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class FortuneController {
+	
+	Logger logger = LoggerFactory.getLogger(FortuneController.class);
+	
+	private String indexName;
+	
+	@Autowired
+	FortuneRepository fortuneRepository;
+
+	@RequestMapping("/random")
+	public Fortune fortune() {
+		logger.info("FortuneController:fortune called ...");
+		
+		Iterable<Fortune> result = fortuneRepository.randomFortunes();
+		
+		return result.iterator().next();
+	}
+
+}
---
+++ spring-oauth2-fortune-service-start/src/main/java/com/thinkenterprise/WebMethodSecurityConfiguration.java
@@ -0,0 +1,17 @@
+package com.thinkenterprise;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
+import org.springframework.security.oauth2.provider.expression.OAuth2MethodSecurityExpressionHandler;
+
+@Configuration
+public class WebMethodSecurityConfiguration extends GlobalMethodSecurityConfiguration {
+	
+	@Override
+	protected MethodSecurityExpressionHandler createExpressionHandler() {
+		return new OAuth2MethodSecurityExpressionHandler();
+	}
+
+}
